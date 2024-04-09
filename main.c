8d7c9ffb6f45dde0bee4fb1a2e43f6366c6c0f01
ergraff <erikgraff39@gmail.com>
2024-04-09 20:17:26 +0200

    Adds bare operations

--- main.c
+++ main.c
@@ -1,4 +1,9 @@
-#include<stdio.h>
+#ifndef STDIO_H
+#include <stdio.h>
+#define STDIO_H
+#endif
+
+#include "op.h"
 
 int main(int argc, char **argv) {
   printf("Hello, world!\n");
---
+++ op.h
@@ -0,0 +1,50 @@
+#include "op.c"
+
+void nop();
+
+void ld();
+void ldh();
+
+void ld_hl();
+void ld_sp();
+
+void add();
+void adc();
+void sub();
+void sbc();
+
+void inc();
+void dec();
+
+void and();
+void or();
+void xor();
+
+void cp();
+void cpl();
+
+void rla();
+void rlca();
+
+void rra();
+void rrca();
+
+void daa();
+
+void scf();
+
+void ccf();
+
+void stop();
+void halt();
+
+void jr();
+void jp();
+
+void call();
+void ret();
+
+void push();
+void pop();
+
+void rst();
