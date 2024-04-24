afae70d0cf457af84cfcbc5a01c3bae5b8f68e1e
ergraff <erikgraff39@gmail.com>
2024-04-24 14:07:48 +0200

    Moves instructions to separate files

---
+++ include/instr.h
@@ -0,0 +1,56 @@
+// -------- Includes --------
+#include "instr.c"
+
+// -------- End of includes --------
+
+// -------- Instructions prototypes --------
+void nop();
+
+void ld();
+void ldh();
+
+void ld_hl();
+void ld_sp();
+
+void add();
+void adc();
+void sub();
+void sbc();
+
+void inc();
+void dec();
+
+void and();
+void or();
+void xor();
+
+void cp();
+void cpl();
+
+void rla();
+void rlca();
+
+void rra();
+void rrca();
+
+void daa();
+
+void scf();
+
+void ccf();
+
+void stop();
+void halt();
+
+void jr();
+void jp();
+
+void call();
+void ret();
+
+void push();
+void pop();
+
+void rst();
+
+// -------- End of instructions prototypes --------
--- src/gb.c
+++ src/gb.c
@@ -4,6 +4,11 @@
 #include <stdint.h>
 #endif
 
+#ifndef INSTR_H
+#define INSTR_H
+#include "instr.h"
+#endif
+
 // -------- End of includes --------
  
 // -------- Memory definitions --------
@@ -127,56 +132,17 @@ uint8_t current_instruction(Gameboy *gb) {
   uint8_t op = gb->mem[addr];
   return op;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 // -------- End of functions --------
-
-// -------- Instructions definitions --------
-void nop() {}
-
-void ld() {}
-void ldh() {}
-
-void ld_hl() {}
-void ld_sp() {}
-
-void add() {}
-void adc() {}
-void sub() {}
-void sbc() {}
-
-void inc() {}
-void dec() {}
-
-void and() {}
-void or() {}
-void xor() {}
-
-void cp() {}
-void cpl() {}
-
-void rla() {}
-void rlca() {}
-
-void rra() {}
-void rrca() {}
-
-void daa() {}
-
-void scf() {}
-
-void ccf() {}
-
-void stop() {}
-void halt() {}
-
-void jr() {}
-void jp() {}
-
-void call() {}
-void ret() {}
-
-void push() {}
-void pop() {}
-
-void rst() {}
-
-// -------- End of instructions definitions --------
