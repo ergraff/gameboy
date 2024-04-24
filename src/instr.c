b055fc8aee58c1cac4836623cc0ba983846640c0
ergraff <erikgraff39@gmail.com>
2024-04-24 14:12:23 +0200

    Removes previous instruction definitions

--- src/instr.c
+++ src/instr.c
@@ -7,53 +7,6 @@
 // -------- End of includes --------
 
 // -------- Instructions definitions --------
-void nop() {}
 
-void ld() {}
-void ldh() {}
-
-void ld_hl() {}
-void ld_sp() {}
-
-void add() {}
-void adc() {}
-void sub() {}
-void sbc() {}
-
-void inc() {}
-void dec() {}
-
-void and() {}
-void or() {}
-void xor() {}
-
-void cp() {}
-void cpl() {}
-
-void rla() {}
-void rlca() {}
-
-void rra() {}
-void rrca() {}
-
-void daa() {}
-
-void scf() {}
-
-void ccf() {}
-
-void stop() {}
-void halt() {}
-
-void jr() {}
-void jp() {}
-
-void call() {}
-void ret() {}
-
-void push() {}
-void pop() {}
-
-void rst() {}
 
 // -------- End of instructions definitions --------
